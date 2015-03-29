abb7836ea9f5ee166dfbcf89ff4bf5ea92bbc0ad
dtysky <dtysky@outlook.com>
2015-03-29 22:42:20 +0800

    Update.
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ WINDOW/RowsCreat/HDL/Rows8x512/Rows8x512.srcs/sources_1/new/Rows8x512.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: None
+// Engineer: Dai Tianyu (dtysky)
+// 
+// Create Date: 2015/03/27 16:45:09
+// Design Name: Rows
+// Module Name: Rows8x512
+// Project Name: Image processing project
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Rows8x512(
+	clk,
+	rst_n,
+	in_color,
+	out_enable,
+	out_color
+	);
+	parameter im_width = 320;
+	parameter rows_width = 5;
+	//Can't be changed in this IP.
+	parameter color_width = 8;
+
+	input clk;
+	input rst_n;
+	input[color_width - 1 : 0] in_color;
+	output out_enable;
+	output[color_width * rows_width - 1 : 0] out_color;
+
+	wire row_rst = ~rst_n;
+	reg reg_row_wr_en[0 : rows_width];
+	wire row_wr_en[0 : rows_width];
+	wire row_rd_en[0 : rows_width - 1];
+	wire[color_width - 1 : 0] row_din[0 : rows_width - 1];
+	wire[color_width - 1 : 0] row_dout[0 : rows_width - 1];
+	wire[8 : 0] row_num[0 : rows_width - 1];
+
+	assign out_enable = row_wr_en[rows_width];
+
+	genvar i;
+	generate
+		for (i = 0; i < rows_width; i = i + 1) begin : Fifos
+			assign row_rd_en[i] = 1 ? row_num[i] == im_width - 1 : 0;
+
+			if (i == 0) begin
+				assign row_din[i] = in_color;
+				assign row_wr_en[i] = 1;
+			end else begin
+				assign row_din[i] = row_dout[i - 1];
+			end
+
+			//One clock delay from fifo read enable to data out
+			always @(posedge clk)
+				reg_row_wr_en[i + 1] <= row_rd_en[i];
+			assign row_wr_en[i + 1] = reg_row_wr_en[i + 1];
+
+			Fifo f(
+				.clk(clk),
+				.rst(row_rst),
+				.din(row_din[i]),
+				.wr_en(row_wr_en[i]),
+				.rd_en(row_rd_en[i]),
+				.dout(row_dout[i]),
+				.data_count(row_num[i])
+				);
+
+			assign out_color[(i + 1) * color_width - 1 : i * color_width] = row_dout[rows_width - 1 - i];
+		
+		end
+
+	endgenerate
+
+endmodule
