96710db37a60b65333f5a2b4859ba67f1745a581
dtysky <dtysky@outlook.com>
2015-05-01 16:00:21 +0800

    Update.
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ POINT/Graying/HDL/RGB2GRAY.srcs/xgui/RGB2GRAY_v1_0.tcl
@@ -0,0 +1,20 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_static_text $IPINST -name "Port discriptions:" -parent ${Page_0} -text {Discriptions:
+
+
+in_enable: Input enable, must enable with the same time as the first input data.
+
+in_data: Input data.
+
+out_enable: Output enable, will be high when the first output data output.
+
+out_data: Output data.}
+
+
+}
+
+
